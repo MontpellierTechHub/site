aa167103f0d6c5978379975aa4da2bddd36d377c
Fabien Bernard <fabien.bernard@tabmo.io>
2017-11-21 20:56:27 +0100

    Create some components and enable HTML5 history mode

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,24 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <mth-header/>
     <router-view/>
+    <mth-footer/>
   </div>
 </template>
 
 <script>
+import MthFooter from '@/components/Footer'
+import MthHeader from '@/components/Header'
+
 export default {
-  name: 'app'
+  name: 'App',
+  components: { MthFooter, MthHeader }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
+  width: 100%;
 }
 </style>
---
+++ src/components/Sponsor.vue
@@ -0,0 +1,14 @@
+<template>
+  <li class="sponsor">{{ sponsor.name }}</li>
+</template>
+
+<script>
+export default {
+  name: 'Sponsor',
+  props: { sponsor: [Object] }
+}
+</script>
+
+<style scoped>
+
+</style>
