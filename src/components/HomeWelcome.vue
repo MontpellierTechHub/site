5cc3a675578f67b7e6038d3518edcdb2bac4875f
Jeremy Barthe <jeremy.barthe@gmail.com>
2018-01-25 10:51:20 +0100

    updated header

--- src/components/HomeWelcome.vue
+++ src/components/HomeWelcome.vue
@@ -2,16 +2,24 @@
   <section class="HomeWelcome">
     <b-container class="position-relative d-table h-100">
       <div class="d-table-cell align-middle text-white text-center">
-        <h1 class="mb-4">Montpellier TechHub</h1>
-        <p>Une super phrase pour nous présenter !</p>
+        <img class="HomeWelcome__icon" :src="icon" alt="Montpellier Tech Hub"/>
+        <h1 class="mb-4">Montpellier <strong>Tech Hub</strong></h1>
+        <p>Association qui accompagne les meetups tech de Montpellier</p>
       </div>
     </b-container>
   </section>
 </template>
 
 <script>
+import icon from '../assets/logo-icon.png'
+
 export default {
-  name: 'HomeWelcome'
+  name: 'HomeWelcome',
+  data () {
+    return {
+      icon: icon
+    }
+  }
 }
 </script>
 
@@ -34,8 +42,18 @@ export default {
     background: #000000;
     opacity: .5;
   }
+  .HomeWelcome h1 {
+    font-size: 2.5rem;
+    font-weight: 100;
+    text-transform: uppercase;
+    letter-spacing: -0.05rem;
+  }
   .HomeWelcome p {
     font-size: 1.5rem;
     font-weight: 300;
+  }
+  .HomeWelcome__icon {
+    width: 5rem;
+    margin-bottom: 1rem;
   }
 </style>
