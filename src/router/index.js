0062b06a5acd53a536e076a15da8aaefb2d804c1
Jeremy Barthe <jeremy.barthe@gmail.com>
2018-02-02 15:41:41 +0100

    scroll to top when browsing links

--- src/router/index.js
+++ src/router/index.js
@@ -41,5 +41,12 @@ export default new Router({
       path: '*',
       redirect: { name: 'home' }
     }
-  ]
+  ],
+  scrollBehavior (to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { x: 0, y: 0 }
+    }
+  }
 })
