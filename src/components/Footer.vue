1aa40ef94467466ee16d0ad1ea89b39b3ffca557
Jeremy Barthe <jeremy.barthe@gmail.com>
2018-02-02 15:41:26 +0100

    updated footer menu

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,24 +3,18 @@
     <b-container>
       <b-row>
         <b-col><img class="footer__logo" :src="logo" alt="Montpellier Tech Hub"/></b-col>
-        <b-col>
+        <b-col cols="8">
           <ul class="list-unstyled">
-            <li><b-nav-item :to="{ name: 'Home'}">Home</b-nav-item></li>
-            <li><b-nav-item :to="{ name:'BecomeMember' }">Devenir member</b-nav-item></li>
+            <li><b-nav-item :to="{ name: 'Home' }">Accueil</b-nav-item></li>
+            <li><b-nav-item :to="{ name: 'BecomeMember' }">Devenir membre</b-nav-item></li>
             <li><b-nav-item target="_blank" href="https://docs.google.com/document/d/1mHhKxdeWkU8H6xXzfF85Hgn9x_InXjjFMb-Cl7AOvqg/edit#">Statuts de l'association</b-nav-item></li>
             <li><b-nav-item href="mailto:montpellier.techhub@gmail.com">Contact</b-nav-item></li>
           </ul>
-
         </b-col>
-        <b-col></b-col>
       </b-row>
       <b-row>
         <p class="copyright">© 2018 Montpellier Tech Hub, association loi 1901</p>
       </b-row>
-      
-      
-
-      
     </b-container>
   </footer>
 </template>
