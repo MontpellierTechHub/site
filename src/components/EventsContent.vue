ca48547683b0b6c226e9272382fc05a911bd56b9
Jeremy Barthe <jeremy.barthe@gmail.com>
2018-03-22 13:22:32 +0100

    improved script to fetch the next Meetup events

--- src/components/EventsContent.vue
+++ src/components/EventsContent.vue
@@ -12,7 +12,7 @@
               </div>
               <div class="event_venue" v-if="event.venue">
                 <h5>{{event.venue.name}}</h5>
-                <span>Adresse</span>
+                <span>{{event.venue.address_1}}</span>
               </div>
             </div>
             <div class="event_details">
--- src/meetup/index.js
+++ src/meetup/index.js
@@ -1,101 +1,58 @@
 const fetch = require('node-fetch');
-const t = require('tcomb');
 const fs = require('fs');
 const path = require('path');
 const meetupApiKey = process.env.MEETUP_API_KEY;
 
 if (meetupApiKey === undefined) {
-    throw new Error('Must provide MEETUP_API_KEY env variable');
+  throw new Error('Must provide MEETUP_API_KEY env variable');
 }
 
-const Group = t.struct({
-    id: t.Number,
-    name: t.String,
-    join_mode: t.maybe(t.String),
-    group_lat: t.Number,
-    group_lon: t.Number,
-    urlname: t.String,
-    who: t.String,
-    created: t.Number,
-}, 'Group');
-
-const Venue = t.struct({
-    id: t.Number,
-    name: t.String,
-    lat: t.Number,
-    lon: t.Number,
-    address1: t.maybe(t.String),
-    address2: t.maybe(t.String),
-    address3: t.maybe(t.String),
-    city: t.String,
-    country: t.String,
-    localized_country_name: t.String,
-}, 'Venue');
-
-const Event = t.struct({
-    id: t.String,
-    created: t.Number,
-    name: t.String,
-    duration: t.maybe(t.Integer),
-    status: t.String,
-    time: t.Number,
-    updated: t.Number,
-    utc_offset: t.Integer,
-    event_url: t.String,
-    description: t.maybe(t.String),
-    visibility: t.String,
-    yes_rsvp_count: t.Integer,
-    maybe_rsvp_count: t.Integer,
-    waitlist_count: t.Integer,
-    headcount: t.Integer,
-    rsvp_limit: t.maybe(t.Integer),
-    venue: t.maybe(Venue),
-    group: Group,
-}, 'Event');
-
 const findUpcomingEvents = async () => {
-    const groupIds = [
-        '19967941', // Big-Data-Science-Montpellier
-        '23234614', // Functional-Programming-Montpellier
-        '18458103', // HumanTalks-Montpellier
-        '19224279', // JUG-Montpellier
-        '26413943', // Meetup-Python-Montpellier
-        '15351542', // MontpellierJS
-        '19216572', // Montpellier-Android-User-Group
-        '17056242', // Montpellier-CPP
-        '18724486', // Montpellier-PHP-Meetup
-    ];
+  const groupIds = [
+    '19967941', // Big-Data-Science-Montpellier
+    '23234614', // Functional-Programming-Montpellier
+    '18458103', // HumanTalks-Montpellier
+    '19224279', // JUG-Montpellier
+    '26413943', // Meetup-Python-Montpellier
+    '15351542', // MontpellierJS
+    '19216572', // Montpellier-Android-User-Group
+    '17056242', // Montpellier-CPP
+    '18724486', // Montpellier-PHP-Meetup
+  ];
 
-    const url = `https://api.meetup.com/2/events?group_id=${groupIds.join()}&status=upcoming&page=100&key=${meetupApiKey}`;
-    const response = await fetch(url);
+  const url = `https://api.meetup.com/2/events?group_id=${groupIds.join()}&status=upcoming&page=100&key=${meetupApiKey}`;
+  const response = await fetch(url);
 
-    if (429 === response.status) {
-        console.log(response.status);
-        console.log(response.headers);
-    }
+  if (429 === response.status) {
+    throw new Error(`Rate limit exceeded:
+    x-meetup-request-id': ${response.headers.get('x-meetup-request-id')}
+    x-ratelimit-limit': ${response.headers.get('x-ratelimit-limit')}
+    x-ratelimit-remaining': ${response.headers.get('x-ratelimit-remaining')}
+    x-ratelimit-reset': ${response.headers.get('x-ratelimit-reset')}`);
+  }
 
-    if (200 !== response.status) {
-        throw new Error('Request failed: ' + response);
-    }
+  if (200 !== response.status) {
+    const body = await response.text();
+    throw new Error(`Request failed: ${response.status} ${response.statusText}
+    x-meetup-request-id': ${response.headers.get('x-meetup-request-id')}
+    ${body}`);
+  }
 
-    const json = await response.json();
+  const json = await response.json();
 
-    return json.results
-        .filter(item => {
-            return 'public_limited' !== item.visibility;
-        })
-        .map(item => {
-            return new Event(item);
-        });
+  return json.results
+    .filter(item => {
+      return 'public_limited' !== item.visibility;
+    });
 };
 
 findUpcomingEvents().then(events => {
-    fs.writeFile(
-        path.join(__dirname, '../assets/events.json'),
-        JSON.stringify(events),
-        (err) => {
-            if (err) throw err;
-            console.log('Done.');
-        }
-    );
+  fs.writeFile(
+    path.join(__dirname, '../assets/events.json'),
+    JSON.stringify(events),
+    (err) => {
+      if (err) throw err;
+      console.log('Done.');
+    }
+  );
 });
