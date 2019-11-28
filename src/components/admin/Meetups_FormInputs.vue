55071130c266bfe4dfbbce19685b90be568711f7
Hugo Gresse <hugo.gresse@gmail.com>
2019-11-28 12:10:40 +0100

    Remove id from meetups form + add description on each field

--- src/components/admin/Meetups_FormInputs.vue
+++ src/components/admin/Meetups_FormInputs.vue
@@ -2,81 +2,78 @@
   <div class="form-column">
     <div class="form-column__left">
       <h3 class="form-title">Informations</h3>
-    
+
       <b-form-input
           class="form"
-          id="meetup-name"
           type="text"
           v-model="form.name"
           required
           placeholder="Name" />
-      
+
       <b-form-textarea
           class="form"
-          id="meetup-description"
           v-model="form.description"
           placeholder="Description de 3 lignes du meetup "
           rows="6"
           max-rows="6"/>
-
+      Logo
       <b-form-input
           class="form"
-          id="meetup-logo"
           type="text"
           v-model="form.logo"
           placeholder="Lien vers un logo" />
-      <b-form-select 
-        class="form" 
-        id="meetup-status" 
-        :options="status" 
-        required 
+      Status
+      <b-form-select
+        class="form"
+        :options="status"
+        required
         v-model="form.status" />
 
       <h3 class="form-title">Représentant</h3>
-      <b-form-select 
-        class="form" 
-        id="meetup-contact-mth" 
-        :options="members"  
+      <b-form-select
+        class="form"
+        :options="members"
         v-model="form.contact_member" />
 
     </div>
-    <div class="form-column__right">  
+    <div class="form-column__right">
       <h3 class="form-title">Statistiques</h3>
+      Nombre d'évènement en moyenne par an
       <b-form-input
           class="form"
-          id="meetup-average_per_year"
           type="text"
           v-model="form.average_per_year"
           placeholder="Nombre d'évènement en moyenne par an" />
-      <datepicker 
-        class="form" 
-        name="last-event" 
-        input-class="datepicker_input" 
-        placeholder="Date du dernier évènement" 
+      Date du dernier évènement
+      <datepicker
+        class="form"
+        name="last-event"
+        input-class="datepicker_input"
+        placeholder="Date du dernier évènement"
         v-model="form.last_event_at"></datepicker>
 
       <h3 class="form-title">Social</h3>
+      Twitter
       <b-form-input
           class="form"
-          id="meetup-meetup_twitter"
           type="text"
           v-model="form.twitter_link"
           placeholder="Url twitter" />
+      Meetup.com url
       <b-form-input
           class="form"
-          id="meetup-meetup_dot_link"
           type="text"
           v-model="form.meetup_dot_link"
           placeholder="Url Meetup.com" />
+      Nombre de membres sur meetup.com
       <b-form-input
           class="form"
-          id="meetup-meetup_dot_members"
           type="text"
           v-model="form.meetup_dot_members"
           placeholder="Nombre de membres sur meetup.com" />
+      Meetup.com id
       <b-form-input
           class="form"
-          id="meetup-meetup_dot_id"
           type="text"
           v-model="form.meetup_dot_id"
           placeholder="Meetup.com id" />
