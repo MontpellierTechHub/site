0e07dd79ef93742f0b8d0a28d8ddd93b007cd7db
Hugo Gresse <hugo.gresse@gmail.com>
2022-06-14 11:21:52 +0200

    Update ical2API route

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,1 @@
-export const ical2ApiURL = 'https://ical2api.web.app/api/v1/events/?status=upcoming&organizations=nXaoPJFpExNpcViswX80'
+export const ical2ApiURL = 'https://ical2api.web.app/api/v1/events/?status=upcoming&organizations=nXaoPJFpExNpcViswX80&readToken=LrHj5Ga77jra1I2kzSnv'
